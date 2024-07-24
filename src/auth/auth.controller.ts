90295999b19424b135597467968442fe235a1508
NohSiHeon <shtlgjs016@naver.com>
2024-07-24 20:14:34 +0900

    feat: 회원가입

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dtos/sign-up.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('1.auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  /**
+   * 회원가입
+   * @param signUpDto
+   * @returns
+   */
+  @Post('/sign-up')
+  async signUp(@Body() signUpDto: SignUpDto) {
+    const data = await this.authService.signUp(signUpDto);
+    return {
+      status: HttpStatus.CREATED,
+      message: '회원가입에 성공했습니다.',
+      data: data,
+    };
+  }
 }
