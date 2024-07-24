f12ab71ad080c2abe62f7478862be904b8a23c48
gus11als <gus11als>
2024-07-24 22:30:10 +0900

    add: 댓글 수정 추가

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, Patch, Param } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 /**
  * 댓글 생성
@@ -19,6 +20,25 @@ export class CommentController {
     return {
       status: HttpStatus.OK,
       message: '댓글을 생성하였습니다.',
+      data,
+    };
+  }
+
+
+  /**
+ * 댓글 수정
+ * @param updateCommentDto 
+ * @returns 수정 댓글 정보와 상태 메시지
+ * */
+  @Patch(':commentId')
+  async update(
+    @Param('commentId') commentId: number,
+    @Body() updateCommentDto: UpdateCommentDto,
+  ): Promise<any> {
+    const data = await this.commentService.update(commentId, updateCommentDto);
+    return {
+      status: HttpStatus.OK,
+      message: "댓글을 수정하였습니다.",
       data,
     };
   }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { Comment } from './entities/comment.entity';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { Post } from 'src/post/entities/post.entity';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Injectable()
 export class CommentService {
@@ -14,6 +15,7 @@ export class CommentService {
     private readonly postRepository: Repository<Post>
   ) {}
 
+  //댓글 생성 api
   async create(createCommentDto: CreateCommentDto): Promise<Comment> {
     // 포스트가 존재하는지 확인
     const post = await this.postRepository.findOne({ where: { id: createCommentDto.postId } });
@@ -22,6 +24,18 @@ export class CommentService {
     }
 
     const comment = this.commentRepository.create(createCommentDto);
+    return this.commentRepository.save(comment);
+  }
+
+
+  //댓글 수정 api
+  async update(commentId: number, updateCommentDto: UpdateCommentDto): Promise<Comment> {
+    const comment = await this.commentRepository.findOne({ where: { id: commentId } });
+    if (!comment) {
+      throw new NotFoundException('댓글을 찾을 수 없습니다.');
+    }
+
+    Object.assign(comment, updateCommentDto);
     return this.commentRepository.save(comment);
   }
 }
