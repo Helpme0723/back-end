0c1e8db34e2d2ba8de05bbb6beea2df611e051b4
lys <projectys29@gmail.com>
2024-07-24 21:53:07 +0900

    feat: 채널 삭제 api 구현

--- src/channel/channel.service.ts
+++ src/channel/channel.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Channel } from './entities/channel.entity';
 import { Repository } from 'typeorm';
@@ -18,11 +18,35 @@ export class ChannelService {
   }
 
   // 채널 상세 조회
-  async findOneChannel(id: number) {
-    const channel = await this.channelRepository.findOneBy({ id });
+  async findOneChannel(channelId: number) {
+    const channel = await this.channelRepository.findOneBy({ id: channelId });
 
     if (!channel) {
       throw new NotFoundException('해당 아이디의 채널이 존재하지 않습니다.');
+    }
+
+    return channel;
+  }
+
+  // 채널 삭제
+  async deleteChannel(userId: number, channelId: number) {
+    const channel = await this.validateChannelOwner(userId, channelId);
+
+    await this.channelRepository.softDelete({ id: channel.id });
+
+    return true;
+  }
+
+  // 채널 삭제 or 수정 권한 검사
+  async validateChannelOwner(userId: number, channelId: number) {
+    const channel = await this.channelRepository.findOneBy({ id: channelId });
+
+    if (!channel) {
+      throw new NotFoundException('해당 아이디의 채널이 없습니다.');
+    }
+
+    if (channel.userId !== userId) {
+      throw new ForbiddenException('권한이 없는 채널입니다.');
     }
 
     return channel;
