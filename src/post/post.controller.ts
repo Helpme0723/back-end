170a2804061eed7310a433d105dd378def30fc4f
minsooC <soochomin@gmail.com>
2024-07-24 18:22:56 +0900

    feat/포스트 전체조회 러프본 완성

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,34 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, Get } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postService.create(createPostDto);
+  async create(@Body() createPostDto: CreatePostDto) {
+    const data = await this.postService.create(createPostDto);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: '',
+      data,
+    };
   }
 
   @Get()
-  findAll() {
-    return this.postService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
+  async findAll() {
+    const data = await this.postService.findAll();
+    return {
+      statusCode: HttpStatus.OK,
+      message: '',
+      data,
+    };
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>
+  ) {}
+
+  async create(createPostDto: CreatePostDto) {
+    const { title, preview, content, price } = createPostDto;
+
+    const post = this.postRepository.create({
+      title,
+      preview,
+      content,
+      price,
+    });
+    return post;
   }
 
-  findAll() {
-    return `This action returns all post`;
-  }
+  async findAll() {
+    const posts = await this.postRepository.find();
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
-
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+    return posts;
   }
 }
