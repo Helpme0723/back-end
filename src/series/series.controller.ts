a8d1f3592b38b78fc36a85608ad1ea244c0a0a84
chominsoo <soochomin@gmail.com>
2024-07-25 13:58:35 +0900

    feat:시리즈생성

--- src/series/series.controller.ts
+++ src/series/series.controller.ts
@@ -1,7 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { SeriesService } from './series.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { UserInfo } from 'src/util/user-info.decorator';
+import { User } from 'src/user/entities/user.entity';
+import { CreateSeriesDto } from './dtos/create-series-dto';
+import { AuthGuard } from '@nestjs/passport';
 
+@ApiTags('시리즈')
 @Controller('series')
 export class SeriesController {
   constructor(private readonly seriesService: SeriesService) {}
+
+  /**
+   * 시리즈 전체조회
+   * @returns
+   */
+  @Get()
+  async findAll() {
+    const data = await this.seriesService.findAll();
+    return {
+      statusCode: HttpStatus.OK,
+      message: '시리즈를 조회하였습니다.',
+      data,
+    };
+  }
+
+  /**
+   * 시리즈 생성
+   * @param user
+   * @param createSeriesDto
+   * @returns
+   */
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  async create(@UserInfo() user: User, @Body() createSeriesDto: CreateSeriesDto) {
+    const userId = user.id;
+
+    const data = await this.seriesService.create(userId, createSeriesDto);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: '시리즈를 생성하였습니다.',
+      data,
+    };
+  }
 }
--- src/series/series.service.ts
+++ src/series/series.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Series } from './entities/series.entity';
+import { Repository } from 'typeorm';
+import { CreateSeriesDto } from './dtos/create-series-dto';
 
 @Injectable()
-export class SeriesService {}
+export class SeriesService {
+  constructor(
+    @InjectRepository(Series)
+    private readonly seriesRepository: Repository<Series>
+  ) {}
+
+  async findAll() {
+    const series = await this.seriesRepository.find();
+    if (!series) {
+      throw new NotFoundException('시리즈 를 찾을수 없습니다.');
+    }
+    return series;
+  }
+
+  async create(userId: number, createSeriesDto: CreateSeriesDto) {
+    const { title, description, channelId } = createSeriesDto;
+
+    const series = await this.seriesRepository.create({
+      userId,
+      title,
+      description,
+      channelId,
+    });
+    await this.seriesRepository.save(series);
+    return series;
+  }
+}
