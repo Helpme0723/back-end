16325bb9773446439d27fb02b57a97bdbc6bbccb
gus11als <gus11als>
2024-07-26 20:49:52 +0900

    add: post구매 1차제작

--- src/purchase/purchase.module.ts
+++ src/purchase/purchase.module.ts
@@ -3,9 +3,11 @@ import { PurchaseService } from './purchase.service';
 import { PurchaseController } from './purchase.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PurchaseList } from './entities/purchase-list.entity';
+import { Post } from 'src/post/entities/post.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PurchaseList])],
+  imports: [TypeOrmModule.forFeature([PurchaseList, Post, User])],
   controllers: [PurchaseController],
   providers: [PurchaseService],
 })
