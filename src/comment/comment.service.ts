b01b8f6f4efc04d3e7b760d4354b4f529fe9be78
gus11als <gus11als>
2024-07-26 19:25:18 +0900

    fix: 튜터님 피드백 수정

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,6 +1,6 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 import { Comment } from './entities/comment.entity';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { Post } from 'src/post/entities/post.entity';
@@ -13,70 +13,60 @@ export class CommentService {
     @InjectRepository(Comment)
     private readonly commentRepository: Repository<Comment>,
     @InjectRepository(Post)
-    private readonly postRepository: Repository<Post>
+    private readonly postRepository: Repository<Post>,
+    private readonly dataSource: DataSource
   ) {}
 
-  //댓글 생성 api
-  async createComment(userId: number, createCommentDto: CreateCommentDto) {
+  // 댓글 생성 API
+  async createComment(createCommentDto: CreateCommentDto & { userId: number }) {
     const post = await this.postRepository.findOne({ where: { id: createCommentDto.postId } });
     if (!post) {
       throw new NotFoundException('포스트를 찾을 수 없습니다.');
     }
 
-    const comment = this.commentRepository.create({ userId, ...createCommentDto });
-
+    const comment = this.commentRepository.create(createCommentDto);
     return this.commentRepository.save(comment);
   }
 
-  //댓글 수정 api
+  // 댓글 수정 API
   async updateComment(userId: number, commentId: number, updateCommentDto: UpdateCommentDto) {
-    const comment = await this.commentRepository.findOne({ where: { id: commentId, userId: userId } });
+    const comment = await this.commentRepository.findOne({ where: { id: commentId, userId } });
     if (!comment) {
       throw new NotFoundException('댓글을 찾을 수 없습니다.');
-    }
-
-    // TODO: dto에서 걸러줘서 빼도 됨
-    const { content } = updateCommentDto;
-    if (!content) {
-      throw new BadRequestException('수정된 내용이 없습니다.');
     }
 
     const updatedComment = await this.commentRepository.save({ id: comment.id, ...updateCommentDto });
     return updatedComment;
   }
 
-  //댓글 삭제 api
+  // 댓글 삭제 API
   async deleteComment(userId: number, commentId: number) {
-    const comment = await this.commentRepository.findOne({ where: { id: commentId, userId: userId } });
+    const comment = await this.commentRepository.findOne({ where: { id: commentId, userId } });
     if (!comment) {
       throw new NotFoundException('댓글을 찾을 수 없습니다.');
     }
 
     await this.commentRepository.softDelete({ id: comment.id });
-
     return true;
   }
 
   // 댓글 좋아요 등록 API
   async createCommentLike(userId: number, commentId: number) {
-    const queryRunner = this.commentRepository.manager.connection.createQueryRunner();
-    //트랜젝션 시작
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
     await queryRunner.startTransaction();
 
     try {
       const comment = await queryRunner.manager.findOne(Comment, { where: { id: commentId } });
 
-      //댓글이 없을시
       if (!comment) {
-        throw new NotFoundException('댓글을 찾을 수 없습니다.');
+        throw new NotFoundException(`댓글을 찾을 수 없습니다. commentId: ${commentId}`);
       }
 
-      // 본인의 댓글인지 확인
       if (comment.userId === userId) {
         throw new BadRequestException('본인의 댓글에는 좋아요를 누를 수 없습니다.');
       }
 
-      // 이미 좋아요를 눌렀는지 확인
       const existingLike = await queryRunner.manager.findOne(CommentLike, { where: { userId, commentId } });
       if (existingLike) {
         throw new BadRequestException('이미 이 댓글에 좋아요를 눌렀습니다.');
@@ -84,34 +74,30 @@ export class CommentService {
 
       const commentLike = queryRunner.manager.create(CommentLike, { userId, commentId });
 
-      // TODO: increment로 변경
-      // likeCount 증가
-      comment.likeCount += 1;
-      await queryRunner.manager.save(Comment, comment);
+      // likeCount 증가를 원자적 연산으로 처리
+      await queryRunner.manager.increment(Comment, { id: commentId }, 'likeCount', 1);
       const savedCommentLike = await queryRunner.manager.save(CommentLike, commentLike);
 
       await queryRunner.commitTransaction();
       await queryRunner.release();
-
-      return savedCommentLike; // 생성된 CommentLike 엔티티 반환
+      return savedCommentLike;
     } catch (error) {
       await queryRunner.rollbackTransaction();
       await queryRunner.release();
-
-      throw error;
+      throw new InternalServerErrorException(`에러: ${error.message}`);
     }
   }
 
   // 댓글 좋아요 삭제 API
   async deleteCommentLike(userId: number, commentId: number) {
-    const queryRunner = this.commentRepository.manager.connection.createQueryRunner();
-
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
     await queryRunner.startTransaction();
 
     try {
       const comment = await queryRunner.manager.findOne(Comment, { where: { id: commentId } });
       if (!comment) {
-        throw new NotFoundException('댓글을 찾을 수 없습니다.');
+        throw new NotFoundException(`댓글을 찾을 수 없습니다. commentId: ${commentId}`);
       }
 
       const commentLike = await queryRunner.manager.findOne(CommentLike, { where: { userId, commentId } });
@@ -119,18 +105,17 @@ export class CommentService {
         throw new NotFoundException('좋아요를 누르지 않았습니다.');
       }
 
-      // TODO: decrement로 수정
-      // likeCount 감소
-      comment.likeCount -= 1;
-      await queryRunner.manager.save(Comment, comment);
+      // likeCount 감소를 원자적 연산으로 처리
+      await queryRunner.manager.decrement(Comment, { id: commentId }, 'likeCount', 1);
       await queryRunner.manager.remove(CommentLike, commentLike);
 
       await queryRunner.commitTransaction();
+      await queryRunner.release();
+      return true;
     } catch (error) {
       await queryRunner.rollbackTransaction();
-      throw error;
-    } finally {
       await queryRunner.release();
+      throw new InternalServerErrorException(`에러: ${error.message}`);
     }
   }
 }
