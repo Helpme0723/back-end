d4328a627e35ad90f98897275c106c8d2f7de3e9
chominsoo <soochomin@gmail.com>
2024-07-29 23:49:59 +0900

    fix/post,series di 추가

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -8,9 +8,10 @@ import { Post } from './entities/post.entity';
 import { Tag } from './entities/tag.entity';
 import { Channel } from 'src/channel/entities/channel.entity';
 import { Series } from 'src/series/entities/series.entity';
+import { PurchaseList } from 'src/purchase/entities/purchase-list.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category, PostLike, Post, Tag, Channel, Series])],
+  imports: [TypeOrmModule.forFeature([Category, PostLike, Post, Tag, Channel, Series, PurchaseList])],
   controllers: [PostController],
   providers: [PostService],
 })
